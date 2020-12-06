65f86e8f56267192d42f2b629fc6b0c99fb9cd0c
Michael Biebl <biebl@debian.org>
2020-12-06 21:57:59 +0100

    New upstream version 1.28.0

--- src/nm-manager.c
+++ src/nm-manager.c
@@ -2445,7 +2445,8 @@ _device_auth_done_fail_on_idle(gpointer user_data, GCancellable *cancellable)
     NMManagerDeviceAuthRequestFunc callback;
     gpointer                       callback_user_data;
 
-    nm_utils_user_data_unpack(&self,
+    nm_utils_user_data_unpack(user_data,
+                              &self,
                               &device,
                               &context,
                               &subject,
@@ -2858,15 +2859,20 @@ recheck_assume_connection(NMManager *self, NMDevice *device)
             activation_type_assume = TRUE;
 
             if (generated) {
+                gs_unref_object NMConnection *con2 = NULL;
+
+                con2 = nm_simple_connection_new_clone(
+                    nm_settings_connection_get_connection(sett_conn));
+
                 /* Reset the IPv4 setting to empty method=auto, regardless of what assumption guessed. */
-                nm_connection_add_setting(nm_settings_connection_get_connection(sett_conn),
+                nm_connection_add_setting(con2,
                                           g_object_new(NM_TYPE_SETTING_IP4_CONFIG,
                                                        NM_SETTING_IP_CONFIG_METHOD,
                                                        NM_SETTING_IP4_CONFIG_METHOD_AUTO,
                                                        NULL));
 
                 nm_settings_connection_update(sett_conn,
-                                              NULL,
+                                              con2,
                                               NM_SETTINGS_CONNECTION_PERSIST_MODE_KEEP,
                                               NM_SETTINGS_CONNECTION_INT_FLAGS_NONE,
                                               NM_SETTINGS_CONNECTION_INT_FLAGS_VOLATILE
